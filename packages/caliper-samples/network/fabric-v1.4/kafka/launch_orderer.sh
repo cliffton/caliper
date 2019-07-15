f8ae76b626c9ee75dde058374c06b272430e73a3
cliffton <cliffton.fernandes.rulz@gmail.com>
2019-07-14 21:25:47 -0400

    launch scrips

--- packages/caliper-samples/network/fabric-v1.4/kafka/launch_orderer.sh
+++ packages/caliper-samples/network/fabric-v1.4/kafka/launch_orderer.sh
@@ -21,7 +21,7 @@ export ORDERER_GENERAL_LISTENADDRESS="0.0.0.0"
 export ORDERER_GENERAL_GENESISMETHOD="file"
 export ORDERER_GENERAL_GENESISFILE="$HOME"/orderer/configtx/genesis.block
 export ORDERER_GENERAL_LOCALMSPID="OrdererMSP"
-export ORDERER_GENERAL_LOCALMSPDIR="$HOME"/orderer/msp/
+export ORDERER_GENERAL_LOCALMSPDIR="$HOME"/orderer/msp/orderer/msp
 export ORDERER_KAFKA_VERBOSE="true"
 export ORDERER_FILELEDGER_LOCATION="$HOME"/orderer/ledger
 export ORDERER_GENERAL_LOGLEVEL="debug"
@@ -30,6 +30,6 @@ export FABRIC_CFG_PATH="$HOME"/orderer
 
 cp orderer.yaml $HOME/orderer/
 cp -r ./config/* $HOME/orderer/configtx/
-cp -r ./config/crypto-config/ordererOrganizations/example.com/orderers/orderer"ORG_ID".example.com/* $HOME/orderer/msp/
+cp -r ./config/crypto-config/ordererOrganizations/example.com/orderers/orderer"ORG_ID".example.com/* $HOME/orderer/msp/orderer/
 cd $HOME/go/src/github.com/hyperledger/fabric/
 orderer
