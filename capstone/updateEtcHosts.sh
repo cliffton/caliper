ddded33b89444da9746b90920041f81296ca0959
cliffton <cliffton.fernandes.rulz@gmail.com>
2019-07-16 21:32:58 -0400

    capstone

---
+++ capstone/updateEtcHosts.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set +x
+
+
+if [ -z $1 ];
+then
+    echo "Please provide the cmd!!!"
+    exit 0
+else
+    START_NODE=$1
+fi
+
+
+if [ -z $2 ];
+then
+    echo  "Please specify start!!!"
+    exit 0
+else
+    END_NODE=$2
+fi
+
+
+if [ -z $3 ];
+then
+    echo  "Please specify cmd!!!"
+    exit 0
+else
+    MY_CMD=$3
+fi
+
+for ip in $(eval echo {$START_NODE..$END_NODE})
+do
+	echo "########### Running $MY_CMD on 10.10.10.$ip #############"
+	ssh atomicpi@10.10.10.$ip $MY_CMD  
+	echo " "
+done
