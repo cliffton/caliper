0495ea8785cf3cb500cbc05e98a1aa653581661b
cliffton <cliffton.fernandes.rulz@gmail.com>
2019-07-19 21:09:48 -0400

    - bug fix for restart scripts.

--- capstone/launchOrderer.sh
+++ capstone/launchOrderer.sh
@@ -32,8 +32,13 @@ fi
 pids=`ps ax | grep -i 'orderer' | grep -v grep | awk '{print $1}'`
 for pid in $(eval echo $pids)
 do
-	echo "########### kill -9 $pid #############"
-	kill -9 $pid
+    if [ $pid == $$ ]
+    then
+        echo "Current PID = " $pid
+    else
+       echo "########### kill -9 $pid #############"
+       kill -9 $pid
+    fi
 done
 
 rm -rf $HOME/orderer
--- capstone/launchPeer.sh
+++ capstone/launchPeer.sh
@@ -42,8 +42,13 @@ fi
 pids=`ps ax | grep -i 'peer' | grep -v grep | awk '{print $1}'`
 for pid in $(eval echo $pids)
 do
-	echo "########### kill -9 $pid #############"
-	kill -9 $pid
+    if [ $pid == $$ ]
+    then
+        echo "Current PID = " $pid
+    else
+       echo "########### kill -9 $pid #############"
+       kill -9 $pid
+    fi
 done
 
 rm -rf $HOME/peer
