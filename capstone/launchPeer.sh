1bca4b40d5f969bdf87152ca3624919f89f650b3
cliffton <cliffton.fernandes.rulz@gmail.com>
2019-07-23 07:49:51 -0400

    launch script fix

--- capstone/launchPeer.sh
+++ capstone/launchPeer.sh
@@ -83,4 +83,4 @@ peer node start &> $HOME/peer/peer.log &
 
 pid=`ps ax | grep -i 'peer node start' | grep -v grep | awk '{print $1}'`
 
-/usr/bin/python3 $HOME/caliper/capstone/processMonitor.py 9001 pid &> $HOME/peer/monitor.log &
+/usr/bin/python3 $HOME/caliper/capstone/processMonitor.py 9001 $pid &> $HOME/peer/monitor.log &
